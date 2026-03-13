4171aa4583f03b97035c0ce1fa0b4c3532fe847d
DioXD-dev <134117738+DioXD-dev@users.noreply.github.com>
2026-03-13 18:38:32 +0700

    Update MainActivity.kt

--- android/app/src/main/java/io/github/dioxd_dev/twa/MainActivity.kt
+++ android/app/src/main/java/io/github/dioxd_dev/twa/MainActivity.kt
@@ -2,7 +2,6 @@ package io.github.dioxd_dev.twa;
 
 import android.os.Bundle
 import androidx.core.view.WindowCompat
-import io.github.dioxd_dev.twa.plugins.MusicScannerPlugin
 import com.getcapacitor.BridgeActivity
 
 class MainActivity : BridgeActivity() {
