1442fcae8aa3049400589a6644b61266077b56ad
DioXD-dev <134117738+DioXD-dev@users.noreply.github.com>
2026-03-13 19:28:25 +0700

    Update MainActivity.kt

--- android/app/src/main/java/io/github/dioxd_dev/twa/MainActivity.kt
+++ android/app/src/main/java/io/github/dioxd_dev/twa/MainActivity.kt
@@ -1,16 +1,10 @@
 package io.github.dioxd_dev.twa
 
 import android.os.Bundle
-import androidx.core.view.WindowCompat
 import com.getcapacitor.BridgeActivity
-import io.github.dioxd_dev.twa.plugins.MusicScannerPlugin
 
 class MainActivity : BridgeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        registerPlugin(MusicScannerPlugin::class.java)
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        window.statusBarColor = android.graphics.Color.TRANSPARENT
-        window.navigationBarColor = android.graphics.Color.TRANSPARENT
     }
 }
